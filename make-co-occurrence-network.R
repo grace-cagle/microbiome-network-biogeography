11c3274f6f965fb1bfa4fdbcd0bca223d4f36d74
grace-cagle <64869945+grace-cagle@users.noreply.github.com>
2022-09-22 14:08:04 -0500

    adjust network params

--- make-co-occurrence-network.R
+++ make-co-occurrence-network.R
@@ -1,28 +1,32 @@
 # A function to calculate a co-occurrence network from a phyloseq object
-# Usage: 
-# make_co_occurence_network(ps, 0.3, 0.05)
+
+# Usage:
 # ps : your phyloseq object
+# corrlevel = 0.6
+# alpha = 0.05
+# make_co_occurence_network(ps, 0.3, 0.05)
 
 library(phyloseq)
 library(biomformat)
 library(psych)
 library(igraph)
 
-make_co_occurrence_network <- function(phyloseq, taxrank, sigrho, alpha) {
+make_co_occurrence_network <- function(phyloseq, corrlevel, alpha) {
   
   dat = data.frame(otu_table(phyloseq))
   
   # calculate correlations
-  cor = corr.test(t(dat),use="pairwise",method="pearson",adjust="BH",alpha=alpha)
+  cor = corr.test(t(dat),use="pairwise",method="spearman",adjust="BH",alpha=alpha)
   cor.r= cor$r 
   cor.p= cor$p 
-  cor.r[cor.p>alpha|cor.r<sigrho] = 0 # select rho and pval
+  cor.r[cor.p>alpha|cor.r<corrlevel] = 0 # select rho and pval
+  # remove non-significant OTUs (e.g., were assigned "0" in prev step)w
+#  cor.r = cor.r[colSums(cor.r) > 0, rowSums(cor.r) > 0]
   # convert the adjacency matrix to a sparse matrix for a faster and more memory-efficient analysis
   cor.r = Matrix(cor.r, sparse = T)
   
   # make network
-  # net = graph_from_adjacency_matrix(cor.r,mode="undirected",weighted=TRUE,diag=FALSE)
-  net = graph_from_adjacency_matrix(cor.r,mode="upper", diag=F)
+  net = graph_from_adjacency_matrix(cor.r,mode="undirected", diag=F)
   
   # # weights cannot be negative for clustering
   # net.weight = E(net)$weight
@@ -48,7 +52,6 @@ make_co_occurrence_network <- function(phyloseq, taxrank, sigrho, alpha) {
                 mean_distance(net),#average path length
                 no.clusters(net),#number.cluster
                 mean(degree(net)/length(V(net))),#normalized degree
-                mean(betweenness(net,normalized=T)),#normalized betweenness
                 centralization.betweenness(net)$centralization, #betweenness centrality
                 average.path.length(net) # average shortest path
   )
@@ -62,7 +65,6 @@ make_co_occurrence_network <- function(phyloseq, taxrank, sigrho, alpha) {
              'mean.distance', 
              'no.clusters',
              'norm.degree',
-             'norm.betweenness',
              'betweenness.centrality',
              'mean.shortest.path'),
              value=round(net.charac, 4)
